dd9337e0bce02f7a6f117eb5ed3dfa48e19e8add
abhishek <abmishra700@gmail.com>
2021-10-16 13:07:05 +0530

    add files and folders

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>hello here</Text>
+    </>
+  );
+};
+
+export default App;
