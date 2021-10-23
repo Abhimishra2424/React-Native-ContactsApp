dedbe914e68db802281f706dc467f1608d9a954d
abhishek <abmishra700@gmail.com>
2021-10-23 08:36:23 +0530

    remove un use files

--- src/navigations/HomeNavigator.js
+++ src/navigations/HomeNavigator.js
@@ -5,7 +5,7 @@ import {Text, View} from 'react-native';
 const ContactsScreen = () => {
   return (
     <View>
-      <Text> hi from contact</Text>
+      <Text> hi from contact Screen</Text>
     </View>
   );
 };
