d486d8e8e558d0f62b7cc2edb170a0510bd397b2
abhishek <abmishra700@gmail.com>
2021-10-27 21:09:33 +0530

    add formik

--- src/components/common/Register/index.js
+++ src/components/common/Register/index.js
@@ -1,14 +1,17 @@
 import { useNavigation } from "@react-navigation/core";
+import { Formik } from "formik";
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
 import colors from "../../../assets/theme/colors";
 import { LOGIN } from "../../../constants/routeNames";
 import Container from "../container";
-import CustomButton from "../CustomButton";
+// import CustomButton from "../CustomButton";
 import Input from "../Input";
+// import * as Yup from "yup";
 
 const RegisterComponent = () => {
+
   const navigation = useNavigation();
   return (
     <Container>
@@ -21,36 +24,69 @@ const RegisterComponent = () => {
       <View>
         <Text style={styles.title}>Welcome to RNCContacts </Text>
         <Text style={styles.subtitle}>New Register here </Text>
-        <View style={styles.form}>
-          <Input
-            label="Username"
-            placeholder="Enter your Name"
-            iconPosition="right"
-          />
-          <Input
-            label="First Name"
-            placeholder="Enter your First Name"
-            iconPosition="right"
-          />
-          <Input
-            label="Last Name"
-            placeholder="Enter your Last Name"
-            iconPosition="right"
-          />
-          <Input
-            label="Email"
-            placeholder="Enter your Email"
-            iconPosition="right"
-          />
-          <Input
-            label="Password"
-            placeholder="Enter your Password"
-            iconPosition="right"
-            secureTextEntry={true}
-            icon={<Text>Show</Text>}
-          />
-        </View>
-        <CustomButton primary title="Submit" />
+
+        <Formik
+          initialValues={{
+            Username: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+          }}
+          onSubmit={(values) => console.log(`values`, values)}
+          validationSchema={SignupSchema}
+        >
+          {({ handleBlur, handleChange, values, handleSubmit}) => (
+            <View style={styles.form}>
+              <Input
+                label="Username"
+                placeholder="Enter your Name"
+                iconPosition="right"
+                onChangeText={handleChange("Username")}
+                onBlur={handleBlur("Username")}
+                value={values.Username}
+              />
+              <Input
+                label="First Name"
+                placeholder="Enter your First Name"
+                iconPosition="right"
+                onChangeText={handleChange("firstName")}
+                onBlur={handleBlur("firstName")}
+                value={values.firstName}    
+              />
+              <Input
+                label="Last Name"
+                placeholder="Enter your Last Name"
+                iconPosition="right"
+                onChangeText={handleChange("lastName")}
+                onBlur={handleBlur("lastName")}
+                value={values.lastName}
+              />
+              <Input
+                label="Email"
+                placeholder="Enter your Email"
+                iconPosition="right"
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+              />
+              <Input
+                label="Password"
+                placeholder="Enter your Password"
+                iconPosition="right"
+                secureTextEntry={true}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                icon={<Text>Show</Text>}
+              />
+              <Button onPress={handleSubmit} title="Submit" mode="contained">
+                submit
+              </Button>
+            </View>
+          )}
+        </Formik>
+
         <View style={styles.createSection}>
           <Text style={styles.infoText}>Already have a an Account? </Text>
           <Button
